eb1af2fd8e3e3359aa29f329ab167cf76d8ce69d
yassine-san <yassineballoutu@gmail.com>
2023-09-16 09:04:30 +0100

    connection done

--- Siemens/users/urls.py
+++ Siemens/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index),
     path('login/', views.index, name="login"),
     path('logUser/', views.logUser, name="logUser"),
+    path('activating_account', views.activating_account, name="activating_user")
 ]
--- Siemens/users/views.py
+++ Siemens/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth import login, logout, authenticate
+from .models import Account
 
 
 def index(request):
@@ -27,6 +28,28 @@ def logUser(request):
         response_data = {'message': message}
         return JsonResponse(response_data)
 
+
+def activating_account(request):
+    if request.method == "POST":
+        message = "nice"
+
+        email = request.POST["email"]
+        pwd = request.POST["password"]
+
+        user = Account.objects.filter(email=email).first()
+        if user:
+            user.is_active = True
+            user.set_password(pwd)
+            user.save()
+            user = authenticate(email=email, password=pwd)
+            if user:
+                login(request, user)
+                return redirect('dashboard:home')
+        else:
+            message = "not nice"
+
+        response_data = {'message': message}
+        return JsonResponse(response_data)
 
 def logoutView(request):
     logout(request)
