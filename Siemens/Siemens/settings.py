042f12fb927a7f6bea36ee6e430bf38c3b284499
yassine-san <yassineballoutu@gmail.com>
2023-09-17 16:59:53 +0100

    not much, but enough

--- Siemens/Siemens/settings.py
+++ Siemens/Siemens/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'Siemens.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'siemensdb',
+        'NAME': 'Siemensdb',
         'USER': 'siemens_pg',
         'PASSWORD': 'root',
     }
